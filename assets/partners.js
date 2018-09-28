6432efc0201b1f04d69ab1415cdffe4088b7a463
Amy Boudsady <aeimskei@gmail.com>
2018-09-27 23:00:00 -0700

    Update Partners with NDG, CCDC and CPTC, remove DHS

--- assets/partners.js
+++ assets/partners.js
@@ -11,39 +11,39 @@ partnersDiv.innerHTML = `
     <!-- first row for partners -->
     <div class="row partner-row">
       <div class="col s2">
-        <a href="https://aws.amazon.com/" target="blank">
-          <img src="img/amazon-aws-logo.png" class="responsive-img partner-img" alt="">
+        <a href="https://www.comptia.org/" target="blank">
+          <img src="img/comptia-logo.png" class="responsive-img partner-img" alt="">
         </a>
       </div>
       <div class="col s2">
-        <a href="https://www.vmware.com/" target="blank">
-          <img src="img/vmware-logo.png" class="responsive-img partner-img" alt="">
+        <a href="https://bavc.org/youth-programs/next-gen" target="blank">
+          <img src="img/bavc-logo.png" class="responsive-img partner-img" alt="">
         </a>
       </div>
       <div class="col s2">
-        <a href="https://www.dellemc.com/en-us/index.htm" target="blank">
-          <img src="img/dell-emc-logo.png" class="responsive-img partner-img" alt="">
+        <a href="http://www.cisco.com/c/en/us/training-events/resources/networking-academy.html" target="blank">
+          <img src="img/cisco-logo.png" class="responsive-img partner-img" alt="">
         </a>
       </div>
       <div class="col s2">
-        <a href="https://azure.microsoft.com/en-us/" target="blank">
-          <img src="img/azure-logo.png" class="responsive-img partner-img" alt="">
+        <a href="http://www.ieee.org/index.html" target="blank">
+          <img src="img/ieee-logo.png" class="responsive-img partner-img" alt="">
         </a>
       </div>
       <div class="col s2">
-        <a href="https://www.splunk.com/" target="blank">
-          <img src="img/splunk-logo.png" class="responsive-img partner-img" alt="">
+      <a href="http://www.juniper.net/us/en/" target="blank">
+        <img src="img/juniper-logo.png" class="responsive-img partner-img" alt="">
+      </a>
+      </div>
+      <div class="col s2">
+        <a href="http://www.iwitts.org/" target="blank">
+          <img src="img/iwitts-logo.png" class="responsive-img partner-img" alt="">
         </a>
       </div>
     </div>
 
     <!-- second row for partners -->
     <div class="row partner-row">
-      <div class="col s2">
-        <a href="http://www.juniper.net/us/en/" target="blank">
-          <img src="img/cisco-logo.png" class="responsive-img partner-img" alt="">
-        </a>
-      </div>
       <div class="col s2">
         <a href="https://www.microsoft.com/en-us/" target="blank">
           <img src="img/microsoft-logo.png" class="responsive-img partner-img" alt="">
@@ -60,8 +60,18 @@ partnersDiv.innerHTML = `
         </a>
       </div>
       <div class="col s2">
-        <a href="https://niccs.us-cert.gov/training/search/city-college-san-francisco" target="blank">
-          <img src="img/dhs-logo.png" class="responsive-img partner-img" alt="">
+        <a href="#" target="blank">
+          <img src="img/ndg-netlab-logo.png" class="responsive-img partner-img" alt="">
+        </a>
+      </div>
+      <div class="col s2">
+        <a href="#" target="blank">
+          <img src="img/ccdc-logo.png" class="responsive-img partner-img" alt="">
+        </a>
+      </div>
+      <div class="col s2">
+        <a href="#" target="blank">
+          <img src="img/cptc-logo.jpg" class="responsive-img partner-img" alt="">
         </a>
       </div>
     </div>
@@ -124,8 +134,18 @@ partnersDiv.innerHTML = `
         </a>
       </div>
       <div class="col s6">
-        <a href="https://niccs.us-cert.gov/training/search/city-college-san-francisco" target="blank">
-          <img src="img/dhs-logo.png" class="responsive-img partner-img-mobile" alt="">
+        <a href="#" target="blank">
+          <img src="img/ndg-netlab-logo.png" class="responsive-img partner-img-mobile" alt="">
+        </a>
+      </div>
+      <div class="col s6">
+        <a href="#" target="blank">
+          <img src="img/ccdc-logo.png" class="responsive-img partner-img-mobile" alt="">
+        </a>
+      </div>
+      <div class="col s6">
+        <a href="#" target="blank">
+          <img src="img/cptc-logo.jpg" class="responsive-img partner-img-mobile" alt="">
         </a>
       </div>
     </div>
